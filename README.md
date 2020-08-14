6d7f346386a5112d23659a2770a7cbd7a5cfef2e
bratukhinmaks <43950639+bratukhinmaks@users.noreply.github.com>
2020-08-14 14:58:17 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# To run progect
+# yarn
+# To build and run 
+#yarn gulp  build lub yarn gulp
---
+++ dist/js/main.js
@@ -0,0 +1,46 @@
+'use strict'; // $("span .slider .round").click(function () {
+//     $(".main-background").toggleClass("main-background-night")
+// });
+
+$('.navbar__hamburger').click(function (e) {
+    $('.navbar__right-side').toggleClass('navbar__right-side--visible');
+    $('.navbar__hamburger').toggleClass('navbar__hamburger--close');
+});
+
+
+
+document.querySelectorAll('.navbar__item').forEach(function (item) {
+    item.addEventListener('click', function (e) {
+        document.querySelector('.navbar__right-side').classList.remove('navbar__right-side--visible');
+        document.querySelector('.navbar__hamburger').classList.remove('navbar__hamburger--close');
+    });
+});
+
+$(document).ready(function () {
+    $('.brand_slider__container').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true });
+
+    $('.feature-slider-items').slick({
+        slidesToShow: 4,
+        responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                arrows: false,
+                slidesToShow: 2 } },
+
+
+        {
+            breakpoint: 480,
+            settings: {
+                dots: true,
+                arrows: false,
+                slidesToShow: 1 } }] });
+
+
+
+
+});
